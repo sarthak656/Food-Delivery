d2cf9e847bfda30fc11e4d1612751b5f4b9d214d
Sarthak Shetty <sarthak.shetty@kambala.co.in>
2024-10-14 16:48:23 +0530

    Destructuring on the fly,ConfigDriven UI,key importance

--- src/UIComponents/rescard.js
+++ src/UIComponents/rescard.js
@@ -1,13 +1,25 @@
-
-const RestaurantCard = (props) =>{
+import React from "react";
+const RestaurantCard = (props) =>{  
+  const { resobj } = props;  //Destructuring the props(destructuring the objects)
+  var list = resobj.restaurants;
+  console.log(list);
     return (
-       <div className="res-card">  
-       <img alt="res-logo" className="res-logo" src='https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/b94ca925fd7a80799b39e5552ca8bf72' />
-       <h2>{props.resname}</h2>
-       <h3>4.4 stars</h3>
-       <h4>30Mins</h4>
-       <h4>{props.cuisines}</h4>
-       </div>
+       <React.Fragment>
+       {list.map((item) => {  //.map() function, the second argument is automatically the index of the current element,
+        let info = item.info;
+        return(
+          <div className="res-card" key={info.id}>  
+       <img alt="res-logo" className="res-logo" src={'https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/' + info.
+cloudinaryImageId} />
+        <h2>{info.name}</h2>
+        <h3>{info.costForTwo}</h3>
+       <h3>{info.avgRating}</h3>
+       <h4>{info.sla.deliveryTime}</h4>
+       <h4>{info.cuisines}</h4> 
+     </div>
+        );
+       })}
+       </React.Fragment>
       );
     }
 export default RestaurantCard
