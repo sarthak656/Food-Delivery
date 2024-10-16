895582104419aa012023b913309b5d43d749348a
Sarthak Shetty <sarthak.shetty@kambala.co.in>
2024-10-16 12:11:48 +0530

    React Hooks useState()

--- src/UIComponents/body.js
+++ src/UIComponents/body.js
@@ -1,19 +1,40 @@
 import RestaurantCard from '../../src/UIComponents/rescard.js';
 import resObj from '../utils/mockdata.js';
+import { useState } from 'react';
+
 const BodyComponent = () =>{
-const ApiList = resObj.restaurants;
+
+const [list,setList ] = useState(resObj.restaurants)
+
+const filterList = () =>{
+   Apilist = ApiList.filter(item =>
+      item.info.avgRating > 4.4,
+      
+      )
+      console.log(Apilist)
+   // setList(list)
+}
+const filter = ()=> {
+   const filteredlist = list.filter(res => res.info.avgRating>4.4)
+   setList(filteredlist)
+}
+
     return (
+      <>
+      <button className='btn-cls' onClick = {filter}>Top Rating Filter</button>
+
        <div className="res-container">
          {
-         ApiList.map((item) => {
+         list.map((item) => {
             return(
             <RestaurantCard 
-            resobj={ item }  key={item.info.id} />
+            resobj={ item.info }  key={item.info.id} />
             )
          }
          )
        }
        </div>
+       </>
     );
   }
 
--- src/UIComponents/rescard.js
+++ src/UIComponents/rescard.js
@@ -2,18 +2,16 @@ import { CDN_URL } from "../utils/constants";  //named imports
 
 const RestaurantCard = (props) =>{  
   const { resobj } = props; //Destructuring the props(destructuring the objects)
-  console.log(resobj); 
-
   //destructuring assignment - and used optional chaining(?. which checks helps prevent runtime errors that can occur when trying to access properties on null or undefined)
-  const {cloudinaryImageId,name,costForTwo,avgRating,cuisines} = resobj?.info
-  const {deliveryTime} = resobj?.info.sla
+  const {cloudinaryImageId,name,costForTwo,avgRating,cuisines} = resobj
+  const {deliveryTime} = resobj?.sla
     return (
       <div className="res-card" >  
        <img alt="res-logo" className="res-logo" src={CDN_URL + cloudinaryImageId} />
        <h2>{name}</h2>
        <h3>{costForTwo}</h3>
        <h3>{avgRating}</h3>
-       <h4>{deliveryTime}</h4>
+       <h4>{deliveryTime}Minutes</h4>
        <h4>{cuisines}</h4> 
      </div>
       );
