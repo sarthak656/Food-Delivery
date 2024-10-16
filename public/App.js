5e816377ab9eb010a6a531b7973281965f1f0079
Sarthak Shetty <sarthak.shetty@kambala.co.in>
2024-10-16 15:11:58 +0530

    hooks, search bar used usestate

--- public/App.js
+++ public/App.js
@@ -3,6 +3,7 @@ import  ReactDOM  from "react-dom/client";
 import logo from "./Assets/images/Logo.png";
 import foodLogo from "./Assets/images/Food_Deliver.png";
 import  AppLayout  from "../src/UIComponents/applayout.js";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
    
--- src/UIComponents/body.js
+++ src/UIComponents/body.js
@@ -1,10 +1,13 @@
 import RestaurantCard from '../../src/UIComponents/rescard.js';
 import resObj from '../utils/mockdata.js';
+
 import { useState } from 'react';
 
 const BodyComponent = () =>{
 
-const [list,setList ] = useState(resObj.restaurants)
+const [list,setList ] = useState(resObj.restaurants);
+const [fulllist,setFullList] = useState(resObj.restaurants);
+
 
 const filterList = () =>{
    Apilist = ApiList.filter(item =>
@@ -18,10 +21,29 @@ const filter = ()=> {
    const filteredlist = list.filter(res => res.info.avgRating>4.4)
    setList(filteredlist)
 }
+ 
+const handleInput= (event) =>{
+console.log(event.target.value)
+const value = event.target.value;
+if(value === ''){
+   setList(fulllist);
+}
+else{
+   const newFilteredList = fulllist.filter(res => res.info.name.toLowerCase().includes(value.toLowerCase())); 
+   setList(newFilteredList);
+}
 
+}
     return (
       <>
+      <div className='row'>
+         <div className='col-lg-6 col-sm-6 col-6'>
+            <input className='input' placeholder='search...' onChange={handleInput}></input>
+         </div>
+         <div className='col-lg-6 col-sm-6 col-6 btn-cont'>
       <button className='btn-cls' onClick = {filter}>Top Rating Filter</button>
+      </div>
+      </div>
 
        <div className="res-container">
          {
